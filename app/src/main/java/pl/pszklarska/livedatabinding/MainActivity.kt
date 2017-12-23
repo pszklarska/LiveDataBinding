d5f7f09b5feee7a19acc8791cd45b26a36aac646
pszklarska <paulina.szklarska@droidsonroids.pl>
2017-12-23 19:47:52 +0100

    Implementing simple data binding

--- app/src/main/java/pl/pszklarska/livedatabinding/MainActivity.kt
+++ app/src/main/java/pl/pszklarska/livedatabinding/MainActivity.kt
@@ -1,12 +1,19 @@
 package pl.pszklarska.livedatabinding
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil.setContentView
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import pl.pszklarska.livedatabinding.databinding.ActivityMainBinding
+import pl.pszklarska.livedatabinding.model.Kitty
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = setContentView(this, R.layout.activity_main)
+        val kitty = Kitty("Fluffy", 3)
+        binding.kitty = kitty
     }
 }
